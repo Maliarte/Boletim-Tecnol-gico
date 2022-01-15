975444687f185aa3e17433bfda5a6db451d7e4f7
Marília S <42978269+Maliarte@users.noreply.github.com>
2022-01-15 10:35:56 -0300

    Versão 1 - Boletim TI
    A intenção desse código foi fazer um MERGE entre os eventos ocorridos na primeira quinzena de Janeiro de 2022.  Altere, Pegue no Colo e deixa um estrelinha se quiseres.

---
+++ Rio_Inovation_Week_Semana_de_ Inovacao_13_14_15_2022_por_maliarte_meid.c
@@ -0,0 +1,165 @@
+/*
+        *              Boletim  MEID para Barbaros Tecnológicos            01.15.2022 
+        *                                                  
+        *                       Desenvolvido por Marília S.
+        *               
+        *             ________________________________________________________________ 
+        *           
+        *                                     |\/\                    
+        *                       
+        *                           https://maliarte.com.br/            
+        *             _______________________________________________________________          
+        *   
+        *
+        * 
+        *   ÍNDICE:
+        * 
+        *   Definição de Tipo: Para quem esse content é destinado...................... 37
+        *
+        *   MCTI e a tecnologia em modo alfa........................................... 64
+        *
+        *   Dois Richard Branson(streamado) na RIW..................................... 86
+        *                                                           
+        *   Maximizar a Tecnologia: isso pode doer..................................... 119
+        *
+        *  Considerações Finais e Agradecimentos........................................ 124
+*/
+
+
+
+/* 
+    Definição de Tipo: Para quem esse content é destinado.
+    
+    
+    Agradeço por fazer desse momento algo estranhamente especial, caras pessoas consumidoras.
+    A Terra tem feito suas requisições. 
+    E a forma em que a mesma manifesta seus anceios nos coloca em posição de Vanguarda.
+
+    De fato, compreender e apreender tem se tornado parte significativa do processo.
+    No meio do caminho,  bugs . Isso é igual para todos. 
+
+    Inspencionem, tenham curiosidade.
+
+    As escolhas para Construção de nosso legado, retornam listas vazias mas declaradas.
+
+    Nós Existimos. E existiremos.
+    
+    preencha-se();
+
+                                                                            Marília Silva.    
+
+
+
+*/
+
+
+
+/*
+    MCTI e a tecnologia em modo alfa.
+    
+    Para o tom que permeia as intenções obtusas, eu insisto: lembre de nossa força, avancemos
+    ruma ao Beta.
+    
+    Caia e fique.
+    É isso que a gravidade diz, quando insiste em manter objetos sobre o Plano.
+    Podemos ir mais longe. É o que diz o potencial de pensar no espaço e seus desdobramentos.
+
+
+    Para àqueles que não se contentam em projetar localmente: eu convido vocês à Ignition. 
+
+    O espaço ausente da informação instantânia, algo se perdeu. 
+
+    Diante desse evento, é preciso toda tentativa de acurar a informação.
+    Não apenas na camada de aplicação, mas em toda a infraestrutura.
+    
+
+*/
+
+
+/* 
+        Dois Richard Branson(streamado) na RIW
+
+
+    Estávamos nós, sincronicamente, em duas casas decimais de presentes online, e outros tantos
+    presencialmente. Era uma sala, a projeção de uma Tela em recursão, apresentava o empresário
+    (#nacionalidade), que faz parte do conjunto dos visionários de um movimento encadeado pelo
+    avanço tecnológico e a escassez de recursos no nosso Planeta.
+
+    Após a reunião, observando pela luneta circular construída pela junção do polegar e o 
+    indicador a lua, retornava a uma época a qual eu não pertencia.
+
+    Me atraía por tudo que me mantivesse intacta e única.
+
+    O mesmo círculo, formado pela imagem e nickname, de uma criança que a live assistia.
+    Era Nicotinha2014.
+    
+    Sentia eu nostalgia, de um momento que ficou para trás, onde tudo era novidade.
+
+    Como um eco, um delay que refletia também o problema técnico que ocorria, sempre, como deve ser.
+
+    Pensava eu, comendo uma comida que improvisei: (batata doce com farofa de amendoim), 
+    -"Fala em cima do dobro, homi". Decici retornar aos afazeres e retornar depois.
+
+    E ainda continuo pensando, sobre escalabilidade. 
+
+    E que o yoda ainda é neném.
+
+
+
+
+*/
+
+/*
+    Maximizar a Tecnologia: isso pode doer
+    
+    Esteja preparado para sofrer. 
+    Você deve começar do NULL, porque é isso que resta.
+    Enquanto passaram anos, da visão idílica da Computação
+    Resta os recortes, que precisam ser remendados.
+
+    Ganhar novas formas e estilos, agregar e não excluir.
+
+
+
+*/
+
+/* 
+
+    Considerações Finais e Agradecimentos
+
+
+    Agradeço primeiramente a Deus, pela oportunidade de aprender e compartilhar.
+    Em seguida, pelo corpo Docente e Discente da FAETERJ-Rio. 
+    Levo cada ensinamento no pensamento e coração. 
+    E nunca menos importantes ao mestres de diferentes partes do globo
+    que dedicam horas de conteúdo de ensino de Programação.
+
+
+
+
+*/
+
+
+
+
+
+
+            /*printf("");
+             printf("");
+              printf("");
+               printf("");
+                printf("");
+                 printf("");
+                  printf("");
+                   printf("");
+                    printf("");
+            
+            printf("======= ISTO É UMA TELA PRETA.========\n");
+            printf("======= O CÓDIGO FOI EXECUTADO.========\n");
+            printf("Escolha Uma Opção:\n");
+
+*/
+
+           
+           
+}
